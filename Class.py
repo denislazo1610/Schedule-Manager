bc59a23deaed17eaea0bb1fbcca0b63b664149b9
denislazo1610 <denis1610@byui.edu>
2021-01-15 15:08:25 -0700

    changes

--- Class.py
+++ Class.py
@@ -13,34 +13,57 @@ class Subject:
             input('Time: '),
             input('Difficulty: '),
             input('Professor: '),
-            int(input('Zoom number: '))
+            input('Zoom number: ')
         )
         
+def options():
+    print("Choose a option:\n")
+    print("\'A\' for adding a subject")
+    print("\'B\' for deleting a subject")
+    print("\'C\' for changing a subject")
+    print("\'D\' for see your schedule")
+    print("\'E\' for write schedule in another file")
+    print("\'Exit\' for exit of this programm\n")
+
+def optionInAction(choice):
+    if((choice == 'A') or (choice == 'a')):
+        user = Subject.from_input()
+        subjects[user.name] = user
+        print("Subject was added to you schedule\n")
+        options()
+        choice = input('Enter your choice:')
+        optionInAction(choice)
+    elif((choice == 'B') or (choice == 'b')):
+        print('BBBBB\n')
+        options()
+        choice = input('Enter your choice:')
+        optionInAction(choice)
+    elif ((choice == 'C') or (choice == 'c')):
+        print('CCCCC\n')
+        options()
+        choice = input('Enter your choice:')
+        optionInAction(choice)
+    elif ((choice == 'D') or (choice == 'd')):
+        print('DDDDD\n')
+        options()
+        choice = input('Enter your choice:')
+        optionInAction(choice)
+    elif ((choice == 'E')) or (choice == 'e'):
+        print('EEEEEE\n')
+        options()
+        choice = input('Enter your choice:')
+        optionInAction(choice)
+    elif ((choice == 'Exit') or (choice == 'exit')):
+        print('Thank you')
+    else:
+        print('Invalid input\n')
+        options()
+        choice = input('Enter your choice:')
+        optionInAction(choice)
+
+print("Hello, this is a Schedule programm!")    
+options()
 choice = input('Enter your choice:')
-
-print("Hello, this is a Schedule programm!")
-print("Choose a option:\n")
-print("\'A\' for adding a subject")
-print("\'B\' for deleting a subject")
-print("\'C\' for changing a subject")
-print("\'D\' for see your schedule")
-print("\'E\' for exit of this programm\n")
-
-
 subjects = {}
-
-if ((choice == 'A') or (choice == 'a')):
-    user = Subject.from_input()
-    subjects[user.name] = user
-    
-elif ((choice == 'B') or (choice == 'b')):
-    print('BBBBB')
-elif ((choice == 'C') or (choice == 'c')):
-    print('CCCCC')
-elif ((choice == 'D') or (choice == 'd')):
-    print('DDDDD')
-elif ((choice == 'E') or (choice == 'e')):
-    print('EEEEE')
-else:
-    print('Invalid input')
+optionInAction(choice)
 
