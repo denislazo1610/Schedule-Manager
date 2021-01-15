26b82f3c92927e0c1306d4e3ddead52317f1e46b
denislazo1610 <denis1610@byui.edu>
2021-01-15 11:26:18 -0700

    Class

--- Class.py
+++ Class.py
@@ -1,1 +1,9 @@
-print("Hello nooo World")
+class Subject:
+    def __init__(self, name, time, difficulty, time, proffesor, zoomNumber):
+        self.name = name
+        self.time = time
+        self.difficulty = difficulty
+        self.time = time
+        self.proffesor = proffesor
+        self.zoomNumber = zoomNumber
+
