26d278b9c5d8c31d7e5ef911859c3e4cbc0a432f
denislazo1610 <denis1610@byui.edu>
2021-01-16 17:42:08 -0700

    Changes

--- Class.py
+++ Class.py
@@ -1,7 +1,8 @@
 class Subject:
-    def __init__(self, name, time, difficulty, professor, zoomNumber):
+    def __init__(self, name, time, days, difficulty, professor, zoomNumber):
         self.name = name
         self.time = time
+        self.days = days
         self.difficulty = difficulty
         self.professor = professor
         self.zoomNumber = zoomNumber
@@ -11,7 +12,8 @@ class Subject:
         return cls(
             input('Name: '),
             input('Time: '),
-            input('Difficulty: '),
+            input('Days: '),
+            input('Difficulty (Hard, Medium, Easy): '),
             input('Professor: '),
             input('Zoom number: ')
         )
@@ -25,7 +27,7 @@ def options():
     print("\'E\' for write schedule in another file")
     print("\'Exit\' for exit of this programm\n")
 
-def optionInAction(choice, subjects):
+def optionInAction(choice, subjects, Schedule):
     if((choice == 'A') or (choice == 'a')):
         user = Subject.from_input()
         if (user.time[1] == ' '):
@@ -39,7 +41,7 @@ def optionInAction(choice, subjects):
         print("Subject was added to you schedule\n")
         options()
         choice = input('Enter your choice:')
-        optionInAction(choice, subjects)
+        optionInAction(choice, subjects, Schedule)
 
     elif((choice == 'B') or (choice == 'b')):
         print('This is all your classes:\n')
@@ -55,7 +57,7 @@ def optionInAction(choice, subjects):
 
         options()
         choice = input('Enter your choice:')
-        optionInAction(choice, subjects)
+        optionInAction(choice, subjects, Schedule)
 
     elif ((choice == 'C') or (choice == 'c')):
         print('This is all your classes:\n')
@@ -67,6 +69,7 @@ def optionInAction(choice, subjects):
             print('This is all the information of that subject:\n')
             print("Name: " + subjects[respuesta].name)
             print("Time: " + subjects[respuesta].time)
+            print("Days: " + subjects[respuesta].days)
             print("Difficulty: " + subjects[respuesta].difficulty)
             print("Professor: " + subjects[respuesta].professor)
             print("Zoom number: " + subjects[respuesta].zoomNumber)
@@ -82,6 +85,8 @@ def optionInAction(choice, subjects):
                 subjects[newInformation] = subjects.pop(respuesta)
             elif ((change == 'Time') or (change == 'time')):
                 subjects[respuesta].time = newInformation
+            elif ((change == 'Days') or (change == 'days')):
+                subjects[respuesta].days = newInformation
             elif ((change == 'Difficulty') or (change == 'difficulty')):
                 subjects[respuesta].difficulty = newInformation
             elif ((change == 'Professor') or (change == 'professor')):
@@ -95,36 +100,44 @@ def optionInAction(choice, subjects):
 
         options()
         choice = input('Enter your choice:')
-        optionInAction(choice, subjects)
+        optionInAction(choice, subjects, Schedule)
     
-    elif ((choice == 'D') or (choice == 'd')):
-        Schedule = [['     ','Monday    ', 'Tuesday    ', 'Wednesday    ','Thursday    ', 'Friday    ', 'Saturday    '] 
-        , ['06 am'],['07 am'],['08 am'],['09 am'],['10 am'], ['11 am'],['12 pm'],['01 pm'],['02 pm'],['03 pm'],['04 pm'],['05 pm']]  
+    elif ((choice == 'D') or (choice == 'd')): 
+
         print('\n')
-        # AQUI LO DEJE
         for key in subjects:
-            print(subjects[key].name)
-            print(subjects[key].time)
+            positionOfTime = subjects[key].time
+            positionOfDay = subjects[key].days
+            for j in range(len(Schedule[0])):
+                if (Schedule[0][j] == positionOfDay):                    
+                    for i in range(len(Schedule)):
+                        if (Schedule[i][0] == positionOfTime):
+                            if(len(subjects[key].name) < len(Schedule[i][j])):
+                                diferencia = (len(Schedule[i][j]) - (len(subjects[key].name)))
+                                Schedule[i][j+1] = (diferencia*' ') + Schedule[i][j+1]
+
+                            Schedule[i][j] = subjects[key].name
+
 
         for i in range(len(Schedule)):
             for j in range(len(Schedule[i])):
                 print(Schedule[i][j], end=' ')
             print()    
         print('\n')
-        correction = input('Do you like your schedule?')
-        if ((correction == 'Yes')or (correction == 'yes')):
-            print('Nice!')
+        correction = input('Do you like your Schedule? (yes/no) ')
+        if ((correction == 'Yes') or (correction == 'yes')):
+            print('Nice!\n')
         else:
             print('You can change it')
         options()
         choice = input('Enter your choice:')
-        optionInAction(choice, subjects)
+        optionInAction(choice, subjects, Schedule)
 
     elif ((choice == 'E')) or (choice == 'e'):
         print('EEEEEE\n')
         options()
         choice = input('Enter your choice:')
-        optionInAction(choice, subjects)
+        optionInAction(choice, subjects, Schedule)
 
     elif ((choice == 'Exit') or (choice == 'exit')):
         print('Thank you')
@@ -133,11 +146,25 @@ def optionInAction(choice, subjects):
         print('Invalid input\n')
         options()
         choice = input('Enter your choice:')
-        optionInAction(choice, subjects)
+        optionInAction(choice, subjects, Schedule)
 
+
+Schedule = [['     ','Monday','    ', 'Tuesday','    ', 'Wednesday','    ','Thursday','    ', 'Friday','    ', 'Saturday','    '] 
+        , ['06 am','      ','    ','       ','    ','         ','    ','        ','    ','      ','    ','        ','    ']
+        ,['07 am','      ','    ','       ','    ','         ','    ','        ','    ','      ','    ','        ','    ']
+        ,['08 am','      ','    ','       ','    ','         ','    ','        ','    ','      ','    ','        ','    ']
+        ,['09 am','      ','    ','       ','    ','         ','    ','        ','    ','      ','    ','        ','    ']
+        ,['10 am','      ','    ','       ','    ','         ','    ','        ','    ','      ','    ','        ','    ']
+        ,['11 am','      ','    ','       ','    ','         ','    ','        ','    ','      ','    ','        ','    ']
+        ,['12 pm','      ','    ','       ','    ','         ','    ','        ','    ','      ','    ','        ','    ']
+        ,['01 pm','      ','    ','       ','    ','         ','    ','        ','    ','      ','    ','        ','    ']
+        ,['02 pm','      ','    ','       ','    ','         ','    ','        ','    ','      ','    ','        ','    ']
+        ,['03 pm','      ','    ','       ','    ','         ','    ','        ','    ','      ','    ','        ','    ']
+        ,['04 pm','      ','    ','       ','    ','         ','    ','        ','    ','      ','    ','        ','    ']
+        ,['05 pm','      ','    ','       ','    ','         ','    ','        ','    ','      ','    ','        ','    ']] 
 print("Hello, this is a Schedule programm!")    
 options()
 choice = input('Enter your choice:')
 subjects = {}
-optionInAction(choice, subjects)
+optionInAction(choice, subjects, Schedule)
 
